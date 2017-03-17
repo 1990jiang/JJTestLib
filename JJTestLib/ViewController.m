b291ff8e08738a450c5330897c43a7bebb812d94
Tmac <562752212@qq.com>
2017-03-17 23:30:01 +0800

    我的框架

---
+++ JJTestLib/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JJTestLib
+//
+//  Created by 蒋俊 on 17/3/17.
+//  Copyright © 2017年 蒋俊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
